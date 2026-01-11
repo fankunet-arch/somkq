1f758bdd418259c16c2f84b61caf01532d274bc0
fankunet-arch <fankunet@gmail.com>
2026-01-12 00:01:53 +0100

    Add files via upload

--- somkq_db_schema_structure_only.sql
+++ somkq_db_schema_structure_only.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： mhdlmskp2kpxguj.mysql.db
--- 生成日期： 2026-01-11 22:52:29
+-- 生成日期： 2026-01-12 00:01:00
 -- 服务器版本： 8.4.6-6
 -- PHP 版本： 8.1.33
 
@@ -55,6 +55,7 @@ CREATE TABLE `somkq_shift_records` (
   `start_time_monitor` time DEFAULT NULL COMMENT '上班时间 (监控时间)',
   `end_time_monitor` time DEFAULT NULL COMMENT '下班时间 (监控时间)',
   `is_end_at_closing` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1=至营业结束',
+  `is_absent` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1=未在岗位出现过',
   `special_tag` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '特殊标记（如：补货、加班等）',
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
