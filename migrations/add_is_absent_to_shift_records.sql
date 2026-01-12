ae21917e0be51d9e8d4d16fd8da737479d42f2c0
fankunet-arch <fankunet@gmail.com>
2026-01-12 12:52:30 +0100

    Delete migrations/add_is_absent_to_shift_records.sql

--- migrations/add_is_absent_to_shift_records.sql
+++
@@ -1,6 +0,0 @@
--- 添加 is_absent 字段到 somkq_shift_records 表
--- 用于标记"未在岗位出现过"的班次记录
-
-ALTER TABLE `somkq_shift_records`
-ADD COLUMN `is_absent` TINYINT(1) NOT NULL DEFAULT '0' COMMENT '1=未在岗位出现过'
-AFTER `is_end_at_closing`;
